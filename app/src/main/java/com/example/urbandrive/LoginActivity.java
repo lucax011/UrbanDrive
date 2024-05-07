c07aca98c02d99e719b67473afa23129c2ac7c0f
Lucas <csdg6924@gmail.com>
2024-05-07 20:01:01 -0300

    Finalizando correções

--- app/src/main/java/com/example/urbandrive/LoginActivity.java
+++ app/src/main/java/com/example/urbandrive/LoginActivity.java
@@ -1,14 +1,15 @@
 package com.example.urbandrive;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.urbandrive.databinding.LoginMainBinding;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
     private LoginMainBinding binding;
 
     @Override
@@ -17,28 +18,23 @@ public class LoginActivity extends Activity {
         binding = LoginMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //  binding.textViewUsername.setText("Nome de usuário");
-
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.login_menu, menu);
         return true;
     }
 
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.login) {
-            Intent loginIntent = new Intent(this, LoginActivity.class);
-            startActivity(loginIntent);
-            return true;
-        } else if (id == R.id.register) {
-            Intent registerIntent = new Intent(this, RegisterActivity.class);
-            startActivity(registerIntent);
-            return true;
-        } else if (id == R.id.request) {
+        if (id == R.id.request) {
             Intent mainIntent = new Intent(this, MainActivity.class);
             startActivity(mainIntent);
             return true;
--- app/src/main/java/com/example/urbandrive/RegisterActivity.java
+++ app/src/main/java/com/example/urbandrive/RegisterActivity.java
@@ -1,14 +1,14 @@
 package com.example.urbandrive;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.example.urbandrive.databinding.RegisterMainBinding;
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends AppCompatActivity {
     private RegisterMainBinding binding;
 
     @Override
@@ -17,26 +17,23 @@ public class RegisterActivity extends Activity {
         binding = RegisterMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //  binding.textViewUsername.setText("Nome de usuário");
     }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.register_menu, menu);
         return true;
     }
 
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.login) {
-            Intent loginIntent = new Intent(this, LoginActivity.class);
-            startActivity(loginIntent);
-            return true;
-        } else if (id == R.id.register) {
-            Intent registerIntent = new Intent(this, RegisterActivity.class);
-            startActivity(registerIntent);
-            return true;
-        } else if (id == R.id.request) {
+        if (id == R.id.request) {
             Intent mainIntent = new Intent(this, MainActivity.class);
             startActivity(mainIntent);
             return true;
